c1b23fca725508061a65d3745f3e9e6af69c82f7
张海锋 <zhang1287609286@qq.com>
2023-05-04 21:55:07 +0800

    changes

--- src/main/java/leetcode/editor/cn/[278]第一个错误的版本.java
+++ src/main/java/leetcode/editor/cn/[278]第一个错误的版本.java
@@ -48,17 +48,9 @@ static
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
-public class Solution extends VersionControl {
+public class Solution {
     public int firstBadVersion(int n) {
-        int start = 1, end = n;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (!isBadVersion(mid))
-                start = mid + 1;
-            else
-                end = mid;
-        }
-        return start;
+        return 1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
